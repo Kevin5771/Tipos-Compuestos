449e9400e56a511f49bb5ba52c2bfacb46bed1f3
kevin571 <kevin577gutierrez@gmail.com>
2023-09-08 22:20:33 -0600

    Cargar Datos

---
+++ Tuplas/tuplas.py
@@ -0,0 +1,11 @@
+#Definir Varias Tuplas e imprimir sus valores
+
+tupla = (1,2,3)
+fecha = (25, "Diciembre", 2016)
+punto =(12, 2)
+persona = ("Gutierrez" , "Kevin", 18)
+
+print(tupla)
+print(fecha)
+print(punto)
+print (persona)
---
+++ index.py
@@ -0,0 +1,5 @@
+versiones_plone = [2.1, 2.5, 3.6, 4, 5, 6]
+print (versiones_plone)
+
+versiones_plone.insert(2, 3.7)
+print (versiones_plone)
